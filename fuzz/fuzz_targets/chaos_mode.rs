a616b415008016865a7bb67814d76d9c2024345b
Remo Senekowitsch <contact@remlse.dev>
2023-02-26 21:03:13 +0100

    fix formatting

--- fuzz/fuzz_targets/chaos_mode.rs
+++ fuzz/fuzz_targets/chaos_mode.rs
@@ -1,9 +1,8 @@
 #![no_main]
-#[macro_use] extern crate libfuzzer_sys;
+#[macro_use]
+extern crate libfuzzer_sys;
 
 use cranelift_codegen::machinst::buffer::test_fuzzing;
 
-// one fuzz target per crate / module / test? 
-fuzz_target!(|data: &[u8]| {
-    test_fuzzing()
-});
+// one fuzz target per crate / module / test?
+fuzz_target!(|data: &[u8]| { test_fuzzing() });
